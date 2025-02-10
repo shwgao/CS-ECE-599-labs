4485201287191510d07a716c10d6f6f49a1b17a5
shouwei-OSU <gaosho@oregonstate.edu>
2025-02-09 21:24:45 -0800

    init commit

---
+++ lab0/test-cpu.py
@@ -0,0 +1,3 @@
+print("\u2764\uFE0F" * 100)
+print("\u2764\uFE0F" * 14 + "  CS/ECE Advanced Topics in Deep Learning & High-performance Computing  " + "\u2764\uFE0F" * 14)
+print("\u2764\uFE0F" * 100)
